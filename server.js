84ae17a7a07b603de771bb8ccfe6f054e818df9a
SirRatul <samsulratul98@gmail.com>
2022-05-17 23:02:20 +0600

    second commit

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const csvtojson = require("csvtojson");
 const cors = require("cors");
 const dotenv = require("dotenv");
-
 dotenv.config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+console.log(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 // middleware
@@ -20,10 +21,28 @@ app.get("/convertToJson", function (req, res) {
     });
 });
 
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
-const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
-
-console.log(stripeSecretKey, stripePublicKey);
+app.post("/payment", cors(), async (req, res) => {
+  let { amount, id } = req.body;
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: "USD",
+      description: "ManakNight Digital",
+      payment_method: id,
+      confirm: true,
+    });
+    console.log("Payment", payment);
+    res.json({
+      payment,
+    });
+  } catch (error) {
+    console.log("Error", error);
+    res.json({
+      message: "Payment failed",
+      success: false,
+    });
+  }
+});
 
 // routers
 const router = require("./routes/orderRouter");
